f84b01ae233e490c69ffb3869bffb5358ccf216d
Abhishek sury1c <abhiiiii15.04@gmail.com>
2025-06-21 11:59:32 +0530

    change head

--- backend/index.js
+++ backend/index.js
@@ -35,9 +35,6 @@ app.use("/api/v1/stories",storyRoute);
 
 
 app.use(express.static(path.join(__dirname,"/frontend/dist")));
-// app.get("*", (req,res)=>{
-//     res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
-// })
 app.get("*", (req, res) => {
   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
 });
@@ -47,53 +44,3 @@ server.listen(PORT,()=>{
     connectDB();
     console.log(`Server is listen at port ${PORT}`);
 })
-
-// import express from 'express'
-// import cors from 'cors'
-// import cookieParser from 'cookie-parser'
-// import dotenv from'dotenv'
-// import connectDB from './utils/db.js';
-// import userRoute from './Routes/user.route.js'
-// import postRoute from './Routes/post.route.js'
-// import storyRoute from './Routes/storyRoutes.js'
-// import messageRoute from './Routes/message.route.js'
-// import { app ,server } from './socket/socket.js';
-// import path from "path";
-
-// dotenv.config({});
-
-
-// const PORT = process.env.PORT || 3000;
-// const __dirname = path.resolve();
-
-// //middleware .env password q7rLDcjeWGOnImRK
-// app.use(express.json());
-// app.use(cookieParser());
-// app.use(express.urlencoded({ extended: true}));
-// const corsOpions = {
-//     origin: 'http://localhost:5173', 
-//     credentials: true 
-// }
-// app.use(cors(corsOpions));
-
-// //Api
-// app.use("/api/v1/user",userRoute);
-// app.use("/api/v1/post",postRoute);
-// app.use("/api/v1/message",messageRoute);
-// app.use("/api/v1/stories",storyRoute);
-
-
-
-// app.use(express.static(path.join(__dirname,"/frontend/dist")));
-// // app.get("*", (req,res)=>{
-// //     res.sendFile(path.resolve(__dirname,"frontend", "dist", "index.html"));
-// // })
-// app.get("*", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
-// });
-
-
-// server.listen(PORT,()=>{
-//     connectDB();
-//     console.log(`Server is listen at port ${PORT}`);
-// })
